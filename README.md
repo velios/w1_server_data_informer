e0baeb88f6778bbcb0e6fad941d087dec4e87105
velios <prostozapomnit@gmail.com>
2017-11-07 23:09:08 +0300

    Write README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-"# w1_server_data_informer" 
+# Server data combiner
+
+Script combine result output from servers, sessions and users files
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+$pip install -r requirements.txt # alternatively try pip3
+```
+
+Put files in root directory and change filepath in `server_data_informer.py` if needed
+```bash
+$python3 server_data_informer.py
+```
--- server_data_informer.py
+++ server_data_informer.py
@@ -2,14 +2,17 @@ import json
 import csv
 import os
 import pandas as pd
-import pprint
+
+# Изменить имена файлов если нужно
+ALL_SERVER_FILE_PATH = '1_allservers.txt'
+SESSIONS_FILE_PATH = '2_sessions.txt'
+USERS_FILE_PATH = '3_users.txt'
 
 
 def load_json_data(filepath):
     if not os.path.exists(filepath):
         return None
     with open(filepath, 'r') as file_handler:
-        # return json.load(file_handler)
         return pd.read_json(file_handler)
 
 
@@ -22,6 +25,7 @@ def load_csv_data_like_json(filepath, delimeter=''):
             csv_rows.extend([{title[i]:row[title[i]] for i in range(len(title))}])
         return csv_rows
 
+
 def fetch_last_sessions_list(sessions_list):
     # Файл сессий читается с конца
     session_id_compare_list, last_sessions_list = [], []
@@ -33,9 +37,9 @@ def fetch_last_sessions_list(sessions_list):
 
 
 def prepare_data():
-    all_servers_dataframe = load_json_data('1_allservers.txt')
-    sessions_list = load_csv_data_like_json('2_sessions.txt', delimeter='\t')
-    users_dataframe = load_json_data('3_users.txt')
+    all_servers_dataframe = load_json_data(ALL_SERVER_FILE_PATH)
+    sessions_list = load_csv_data_like_json(SESSIONS_FILE_PATH, delimeter='\t')
+    users_dataframe = load_json_data(USERS_FILE_PATH)
     last_sessions_list = fetch_last_sessions_list(sessions_list)
     last_sessions_dataframe = pd.read_json(json.dumps(last_sessions_list))
     prepare_list_of_dicts = last_sessions_dataframe.merge(users_dataframe,
@@ -52,7 +56,6 @@ def prepare_data():
 def print_last_session_machine_list(final_list_of_dicts):
     print('{:<3}  {:<40}    {:<40}   {:<30}   {:<20}   {:<70}   {:<30}'.format('№', 'Session_№', 'Server_id', 'Server_name',
                                                                       'Host','User_Id', 'User_name'))
-
     for index, server_data in enumerate(final_list_of_dicts, start=1):
         print(
             '{index:<3}: {session_id:<40} => {server_id:<40} - {server_name:<30} - {host:<20} - {user_id:<70} - {user_name:<30}'.format(
