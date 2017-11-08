0f2dc116b4db289627b71f2265841236b1571d5c
velios <prostozapomnit@gmail.com>
2017-11-08 23:12:40 +0300

    Remove pandas dependency

--- server_data_informer.py
+++ server_data_informer.py
@@ -1,7 +1,7 @@
 import json
 import csv
 import os
-import pandas as pd
+import pprint
 
 # Изменить имена файлов если нужно
 ALL_SERVER_FILE_PATH = '1_allservers.txt'
@@ -13,10 +13,10 @@ def load_json_data(filepath):
     if not os.path.exists(filepath):
         return None
     with open(filepath, 'r') as file_handler:
-        return pd.read_json(file_handler)
+        return json.load(file_handler)
 
 
-def load_csv_data_like_json(filepath, delimeter=''):
+def load_csv_data_in_json_format(filepath, delimeter=''):
     csv_rows = []
     with open(filepath) as csvfile:
         reader = csv.DictReader(csvfile, delimiter='\t')
@@ -36,39 +36,31 @@ def fetch_last_sessions_list(sessions_list):
     return last_sessions_list
 
 
-def prepare_data():
-    all_servers_dataframe = load_json_data(ALL_SERVER_FILE_PATH)
-    sessions_list = load_csv_data_like_json(SESSIONS_FILE_PATH, delimeter='\t')
-    users_dataframe = load_json_data(USERS_FILE_PATH)
-    last_sessions_list = fetch_last_sessions_list(sessions_list)
-    last_sessions_dataframe = pd.read_json(json.dumps(last_sessions_list))
-    prepare_list_of_dicts = last_sessions_dataframe.merge(users_dataframe,
-                                                          left_on='user_id',
-                                                          right_on='ID',
-                                                          how='left')
-    final_list_of_dicts = prepare_list_of_dicts.merge(all_servers_dataframe,
-                                                      left_on='instance_id',
-                                                      right_on='ID',
-                                                      how='left').T.to_dict().values()
-    return final_list_of_dicts
-
-
-def print_last_session_machine_list(final_list_of_dicts):
-    print('{:<3}  {:<40}    {:<40}   {:<30}   {:<20}   {:<70}   {:<30}'.format('№', 'Session_№', 'Server_id', 'Server_name',
-                                                                      'Host','User_Id', 'User_name'))
-    for index, server_data in enumerate(final_list_of_dicts, start=1):
-        print(
-            '{index:<3}: {session_id:<40} => {server_id:<40} - {server_name:<30} - {host:<20} - {user_id:<70} - {user_name:<30}'.format(
-                index=index,
-                session_id=server_data['session_id'],
-                server_id=server_data['ID_y'],
-                server_name=server_data['Name_y'],
-                host=server_data['Host'],
-                user_name=server_data['Name_x'],
-                user_id=server_data['ID_x']
-                ))
+def merge_list_of_dicts(list_1, list_2, key_1, key_2):
+    result_list = []
+    for el_1 in list_1:
+        for el_2 in list_2:
+            if el_1[key_1] == el_2[key_2]:
+                result_list.append(dict(el_1, **el_2))
+                break
+        else:
+            result_list.append(el_1)
+    return result_list
 
 
 if __name__ == '__main__':
-    final_list_of_dicts = prepare_data()
-    print_last_session_machine_list(final_list_of_dicts)
+    all_servers_list = load_json_data(ALL_SERVER_FILE_PATH)
+    sessions_list = load_csv_data_in_json_format(SESSIONS_FILE_PATH,
+                                                 delimeter='\t')
+    users_list = load_json_data(USERS_FILE_PATH)
+    last_sessions_list = fetch_last_sessions_list(sessions_list)
+
+    # print(all_servers_list)
+    # pprint.pprint(sessions_list)
+    # print(users_dataframe)
+    # print(last_sessions_list)
+    # print(len(last_sessions_list))
+    first_merge = merge_list_of_dicts(last_sessions_list, all_servers_list, key_1='instance_id', key_2='ID')
+    pprint.pprint(merge_list_of_dicts(first_merge, users_list, key_1='user_id', key_2='ID'))
+    # print(len(first_merge))
+    # pprint.pprint(first_merge)
